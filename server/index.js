cfaa36ce8c9694d7f0d0bde035de2f16cf35498d
Heidi Poon <heidixpoon@gmail.com>
2018-04-24 23:18:55 -0400

    deploy

--- server/index.js
+++ server/index.js
@@ -144,7 +144,7 @@ app.get('/*', (req, res) => {
 })
 
 
-let port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080;
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
